a73df4b05a698ee85eb7a4967405ec10daf8d749
manoj singh <manoj_243@yahoo.in>
2025-03-05 18:33:52 +0530

    antu-05-2

---
+++ BizSolESMS/Bizsol_ESMS/Controllers/ReportController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bizsol_ESMS.Controllers
+{
+    public class ReportController : Controller
+    {
+        public IActionResult LocationReport()
+        {
+            return View();
+        }
+    }
+}
